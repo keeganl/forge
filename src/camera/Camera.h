ed5a9bbcd167664dc7bfb26e6d60367e666852be
keeganl <keeganlk.7@gmail.com>
2021-06-08 00:05:34 -0500

    3d and camera

---
+++ src/camera/Camera.h
@@ -0,0 +1,47 @@
+#ifndef FORGE_CAMERA_H
+#define FORGE_CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// passing the data to imgui
+struct Camera {
+    float fov{45.0f};
+    float speed{2.5f};
+    glm::vec3 pos{glm::vec3(0.0f, 0.0f, 3.0f)};
+    glm::vec3 target{glm::vec3(0.0f, 0.0f, 0.0f)};
+    glm::vec3 direction{glm::normalize(glm::vec3(0.0f, 0.0f, 3.0f) - glm::vec3(0.0f, 0.0f, 0.0f))};
+    glm::vec3 front{glm::vec3(0.0f, 0.0f, -1.0f)};
+    glm::vec3 up{glm::vec3(0.0f, 1.0f,  0.0f)};
+
+    Camera() {}
+
+    Camera(float &camFov, float &camSpeed, glm::vec3 &camPos, glm::vec3 &camTarget, glm::vec3 &camDirection, glm::vec3 &camFront, glm::vec3 &camUp) :
+            fov(camFov),
+            speed(camSpeed),
+            pos(camPos),
+            target(camTarget),
+            direction(glm::normalize(camPos - camTarget)),
+            front(camFront),
+            up(camUp)
+    {}
+
+//    float& getFov();
+//    void setFov(float &newFov);
+//    float& getSpeed();
+//    void setSpeed(float &newSpeed);
+//    glm::vec3& getPosition();
+//    void setPosition(glm::vec3 &newPos);
+//    glm::vec3& getTarget();
+//    void setTarget(glm::vec3 &newTarget);
+//    glm::vec3& getDirection();
+//    void setDirection(glm::vec3 &newDirection);
+//    glm::vec3& getFront();
+//    void setFront(glm::vec3 &newFront);
+//    glm::vec3& getUp();
+//    void setUp(glm::vec3 &newUp);
+};
+
+
+#endif //FORGE_CAMERA_H
--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,14 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
-#include "utils/imgui-layer/ImGuiLayer.h"
+#include "utils/imgui-layer/GuiLayer.h"
 #include "utils/shader-manager/Shader.h"
+#include "camera/Camera.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -45,9 +50,6 @@ int main()
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-
-    ImGuiLayer imGuiLayer = ImGuiLayer();
-
     // glfw window creation
     // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Forge", NULL, NULL);
@@ -69,59 +71,88 @@ int main()
         return -1;
     }
 
-    //load shader
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile our shader zprogram
+    // ------------------------------------
     Shader ourShader("../shaders/vert.glsl", "../shaders/frag.glsl");
 
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-    unsigned int indices[] = {  // note that we start from 0!
-            0, 1, 3,  // first Triangle
-            1, 2, 3   // second Triangle
+    // world space positions of our cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
     // texture coord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
-
-
-    // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     //load texture
     // load and create a texture
@@ -132,15 +163,14 @@ int main()
     glGenTextures(1, &texture1);
     glBindTexture(GL_TEXTURE_2D, texture1);
     // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters (i.e. mipmapping)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
     unsigned char *data = stbi_load("../assets/textures/tiles.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -156,11 +186,10 @@ int main()
 
     // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
     // -------------------------------------------------------------------------------------------
-    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
-    // either set it manually like so:
-    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
 
-    imGuiLayer.createContext(window);
+    GuiLayer::createContext(window);
 
     // Our state
     [[maybe_unused]] bool show_demo_window = true;
@@ -170,17 +199,40 @@ int main()
 
     float scale = 1.0f;
     float mix = 0.2f;
+    float nearClipping = 0.1f;
+    float farClipping = 100.0f;
+    // todo: class to manage
+    bool orthographic = false;
+    int w = 87;
+    int a = 65;
+    int s = 83;
+    int d = 68;
+
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
+
+
+
     ImVec4 color = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+
+    Camera camera = Camera();
+
+    glm::vec3 initialCamPos = camera.pos;
 
     ourShader.use();
     ourShader.set1DFloat("scale", scale);
     ourShader.set1DFloat("mixVal", mix);
-    ourShader.set4DFloat("color",  color.x, color.y, color.z, color.w);\
+//    ourShader.set3DFloat("translate", test[0], test[1], test[2]);
+    ourShader.set4DFloat("color",  color.x, color.y, color.z, color.w);
 
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         // input
         // -----
@@ -189,39 +241,44 @@ int main()
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // also clear the depth buffer now!
 
         // bind textures on corresponding texture units
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
 
 
-        // draw our first triangle
-        ourShader.use();
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
-        if (draw)
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        // glBindVertexArray(0); // no need to unbind it every time
 
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        if (orthographic) {
+            projection = glm::ortho(0.0f, (float) SCR_WIDTH, 0.0f, (float) SCR_HEIGHT, nearClipping, farClipping);
+        } else {
+            projection = glm::perspective(glm::radians(camera.fov), (float) SCR_WIDTH / (float) SCR_HEIGHT, nearClipping, farClipping);
+        }
+        view = glm::lookAt(camera.pos, camera.pos + camera.front, camera.up);
+        // pass transformation matrices to the shader
 
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
+        // render boxes
+        glBindVertexArray(VAO);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
 
-        imGuiLayer.createDockspace();
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
-        ImGui::Begin("Performance window");
-        ImGui::Text("Application average (%.0f FPS)", ImGui::GetIO().Framerate);
-        ImGui::End();
+        GuiLayer::startFrame();
+        GuiLayer::createDockspace();
+        GuiLayer::createPerformanceWindow();
+        ImGui::ShowMetricsWindow(&show_demo_window);
 
-//        ImGui::ShowDemoWindow(&show_demo_window);
-
-        ImGui::Begin("Geometry Manager");
-        ImGui::Text("click to draw geometry!");
-        ImGui::Checkbox("Click here", &draw);
-        ImGui::Text("click to draw geometry!");
+        ImGui::Begin("Geometry Properties");
         ImGui::SliderFloat("Scale", &scale, -10.0f, 10.0f);
         ImGui::Text("Color widget:");
         ImGui::SameLine(); HelpMarker(
@@ -231,6 +288,41 @@ int main()
         ImGui::SliderFloat("Mix", &mix, 0.0f, 1.0f);
         ImGui::End();
 
+        ImGui::Begin("Camera Properties");
+        ImGui::Text("Select projection mode:");
+        ImGui::Text("Swap between orthographic and projection:");
+        ImGui::SameLine(); HelpMarker(
+                "Perspective is default.\n");
+        ImGui::Checkbox("Click here", &orthographic);
+        ImGui::SliderFloat("FOV", &camera.fov, 45.0f, 100.0f);
+        ImGui::DragFloat3("Camera Pos", &camera.pos[0]);
+        ImGui::Button("Reset camera", ImVec2(150, 50));
+
+        float frameCamSpeed = camera.speed * deltaTime;
+        if (ImGui::IsKeyPressed(w)) {
+
+            camera.pos += frameCamSpeed * camera.front;
+        }
+        if (ImGui::IsKeyPressed(a)) {
+            camera.pos -= frameCamSpeed * glm::normalize(glm::cross(camera.front, camera.up));
+        }
+        if (ImGui::IsKeyPressed(s)) {
+            camera.pos -= frameCamSpeed * camera.front;
+
+        }
+        if (ImGui::IsKeyPressed(d)) {
+            camera.pos += frameCamSpeed * glm::normalize(glm::cross(camera.front, camera.up));
+        }
+
+
+        if (io.MouseWheel < 0 && camera.fov < 100.0f) {
+            camera.fov = camera.fov + 5.0f;
+        }
+        if (io.MouseWheel > 0 && camera.fov > 45.0f) {
+            camera.fov = camera.fov - 5.0f;
+        }
+
+        ImGui::End();
         ImGui::Begin("Events");
         ImGui::Text("Keys down:");          for (int i = 0; i < IM_ARRAYSIZE(io.KeysDown); i++) if (ImGui::IsKeyDown(i))        { ImGui::SameLine(); ImGui::Text("%d (0x%X) (%.02f secs)", i, i, io.KeysDownDuration[i]); }
         ImGui::Text("Keys pressed:");       for (int i = 0; i < IM_ARRAYSIZE(io.KeysDown); i++) if (ImGui::IsKeyPressed(i))     { ImGui::SameLine(); ImGui::Text("%d (0x%X)", i, i); }
@@ -240,14 +332,6 @@ int main()
 
         ImGui::Text("NavInputs down:");     for (int i = 0; i < IM_ARRAYSIZE(io.NavInputs); i++) if (io.NavInputs[i] > 0.0f)              { ImGui::SameLine(); ImGui::Text("[%d] %.2f (%.02f secs)", i, io.NavInputs[i], io.NavInputsDownDuration[i]); }
         ImGui::Text("NavInputs pressed:");  for (int i = 0; i < IM_ARRAYSIZE(io.NavInputs); i++) if (io.NavInputsDownDuration[i] == 0.0f) { ImGui::SameLine(); ImGui::Text("[%d]", i); }
-
-        ImGui::Button("Hovering me sets the\nkeyboard capture flag");
-        if (ImGui::IsItemHovered())
-            ImGui::CaptureKeyboardFromApp(true);
-        ImGui::SameLine();
-        ImGui::Button("Holding me clears the\nthe keyboard capture flag");
-        if (ImGui::IsItemActive())
-            ImGui::CaptureKeyboardFromApp(false);
 
         if (ImGui::IsMousePosValid())
             ImGui::Text("Mouse pos: (%g, %g)", io.MousePos.x, io.MousePos.y);
@@ -259,13 +343,14 @@ int main()
         ImGui::Text("Mouse dblclick:"); for (int i = 0; i < IM_ARRAYSIZE(io.MouseDown); i++) if (ImGui::IsMouseDoubleClicked(i)){ ImGui::SameLine(); ImGui::Text("b%d", i); }
         ImGui::Text("Mouse released:"); for (int i = 0; i < IM_ARRAYSIZE(io.MouseDown); i++) if (ImGui::IsMouseReleased(i))     { ImGui::SameLine(); ImGui::Text("b%d", i); }
         ImGui::Text("Mouse wheel: %.1f", io.MouseWheel);
-        ImGui::Text("Pen Pressure: %.1f", io.PenPressure); // Note: currently unused
         ImGui::End();
 
         ourShader.use();
         ourShader.set1DFloat("scale", scale);
         ourShader.set1DFloat("mixVal", mix);
         ourShader.set4DFloat("color",  color.x, color.y, color.z, color.w);
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -286,17 +371,13 @@ int main()
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
-
-
-
     }
-
 
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+
     ourShader.destroy();
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
