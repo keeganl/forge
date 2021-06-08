dcbdb810b9bdcbbaa9be155656988ff1e47a4155
keeganl <keeganlk.7@gmail.com>
2021-06-08 00:05:52 -0500

    3d in vertex shader

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -3,9 +3,13 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
 uniform float scale;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 out vec2 TexCoord;
 void main()
 {
-  gl_Position = vec4(scale * aPos.x, scale * aPos.y, scale * aPos.z, 1.0);
+
+  gl_Position = projection * model * view * vec4(scale * aPos.x, scale * aPos.y, scale * aPos.z, 1.0);
   TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
